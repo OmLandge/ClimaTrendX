863bf29864fd171d2fc359fca2432806ee496d6c
Om Landge <omlandge0000@gmail.com>
2025-02-26 14:35:39 +0530

    fixed router logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,18 @@
 require("dotenv").config();
 import express from "express";
 import cors from "cors";
+import WeatherRouter from "./routes/weather";
+import AirRouter from "./routes/air";
+import AIRouter from "./routes/ai";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/v1/weather", WeatherRouter);
+app.use("/api/v1/air", AirRouter);
+app.use("/api/v1/ai", AIRouter);
 
 app.get("/", (req, res) => {
   res.send("ClimaTrendX Backend!");
